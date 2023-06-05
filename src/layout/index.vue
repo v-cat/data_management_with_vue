1c4f09947eb191e2891973098e8db9e89fde43f9
maself-design <1936881152@qq.com>
2023-06-05 18:41:32 +0800

    样式布局调整

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,19 +1,14 @@
 <template>
   <div class="container">
     <div class="right">
-      <div class="top"> 
-        top
-      </div>
-      <div class="main">  <router-view></router-view></div>
-      <div  class="footer">footer</div>
+      <div class="top">top</div>
+      <div class="main"><router-view></router-view></div>
+      <div class="footer">footer</div>
     </div>
-
-
   </div>
 </template>
 
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <style>
 .container {
@@ -23,30 +18,32 @@
   display: flex;
   flex-wrap: nowrap;
 }
-.container .right{
+.container .right {
   width: 100%;
+  display: flex;
+  flex-direction: column;
 }
-.container .right .top{
-  width: 100%;  
+.container .right .top {
+  width: 100%;
   height: 50px;
   background: white;
   text-align: center;
   border-bottom: 1px solid rgb(229 229 229);
   line-height: 50px;
-
 }
-.container .right .main{
+.container .right .main {
   padding: 40px;
-  width: 100%;  
-  height: calc(100vh - 100px);
-  background: #f2fff6
+  width: 100%;
+  flex: 1;
+  /* height: calc(100vh - 100px); */
+  background: #f2fff6;
 }
-.container .right .footer{
-  width: 100%;  
+.container .right .footer {
+  width: 100%;
   height: 50px;
   position: absolute;
   bottom: 0;
-  background:rgb(229 229 229);
+  background: rgb(229 229 229);
   text-align: center;
   line-height: 50px;
 }
